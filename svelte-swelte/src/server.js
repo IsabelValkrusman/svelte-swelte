8bb686687d384c5e114dc622cd1f991baba38c06
Opilane TTHK <opilane@tthk.ee>
2023-10-06 09:37:06 +0300

    Initial commit

---
+++ svelte-swelte/src/server.js
@@ -0,0 +1,34 @@
+import sirv from 'sirv';
+import polka from 'polka';
+import compression from 'compression';
+import * as sapper from '@sapper/server';
+const {json} = require('body-parser');
+import session from 'express-session';
+import sessionFileStore from 'session-file-store'
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
+const FileStore = sessionFileStore(session);
+
+
+polka() // You can also use Express
+	.use(
+		compression({ threshold: 0 }),
+		json(),
+		sirv('static', { dev }),
+		sapper.middleware({
+			secret: 'svelte',
+			resave: false,
+			saveUninitialized: true,
+			cookie: {
+				maxAge: 31536000
+			},store: new FileStore({
+				path: `.sessions`
+			})
+		})
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+
+	});
